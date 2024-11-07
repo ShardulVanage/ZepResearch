2b4f19d1862f5efc7cb3eb91b5d56eb472c6caa3
ShardulVanage <vsharadul@gmail.com>
2024-11-07 19:11:29 +0530

    gallery added

--- frontend/src/pages/Solution Menu/Gallery.jsx
+++ frontend/src/pages/Solution Menu/Gallery.jsx
@@ -1,43 +1,152 @@
-
-import g1 from '../../images/gallery/1.jpg'
-import g2 from '../../images/gallery/2.jpg'
-import g3 from '../../images/gallery/3.jpg'
-import g4 from '../../images/gallery/4.jpg'
-import g5 from '../../images/gallery/5.jpg'
-import g6 from '../../images/gallery/6.jpg'
-import g7 from '../../images/gallery/7.jpg'
-import g8 from '../../images/gallery/8.jpg'
-import g9 from '../../images/gallery/9.jpg'
-import g10 from '../../images/gallery/10.jpg'
-import g11 from '../../images/gallery/11.jpg'
-import g12 from '../../images/gallery/12.jpg'
-import g13 from '../../images/gallery/13.jpg'
-
-import g16 from '../../images/gallery/16.jpeg'
-import React, { useEffect, useRef } from 'react';
-import { motion } from 'framer-motion';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import GalleryItem from './GalleryItem';
+import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import GalleryItem from "./GalleryItem";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const galleryData = [
-  { id: 1,  imageUrl: g1,},
-  { id: 2,  imageUrl: g2,},
-  { id: 3,  imageUrl: g3,},
-  { id: 4,  imageUrl: g4,},
-  { id: 5,  imageUrl: g5,},
-  { id: 6,  imageUrl: g6,},
-  { id: 7,  imageUrl: g7,},
-  { id: 8,  imageUrl: g8,},
-  { id: 9,  imageUrl: g9,},
-  { id: 10,  imageUrl: g10,},
-  { id: 11,  imageUrl: g11,},
-  { id: 12,  imageUrl: g12,},
-  { id: 13,  imageUrl: g13,},
-  
- 
+  {
+    id: 1,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876302/Gallery/v3oashc1hzwihilxf3gm.jpg",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876297/Gallery/jdf8gyetdgiedxlhnpnl.jpg",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876296/Gallery/lkcyqy7rk2p0e6oesswk.jpg",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876296/Gallery/mxivsfp3g0y0n3eaiq44.jpg",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876296/Gallery/yhlhxotliva9utsfysn3.jpg",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876296/Gallery/p2tvfkmhq5qgoxcuyjdr.jpg",
+  },
+  {
+    id: 7,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876295/Gallery/jxouvaqolvravdgpj5rl.jpg",
+  },
+  {
+    id: 8,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876295/Gallery/pa3i8esjgvq48mknllcx.jpg",
+  },
+  {
+    id: 9,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876295/Gallery/rrsb08xchyicddz6qshx.jpg",
+  },
+  {
+    id: 10,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/fmrpgxt6zw7kidnw9ccj.jpg",
+  },
+  {
+    id: 11,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/bdizrg7nkr31qzrm6j2b.jpg",
+  },
+  {
+    id: 12,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/hg4u0klfxydvar2mpgjo.jpg",
+  },
+  {
+    id: 13,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/alqwdo3c2hqw2fa0sywn.jpg",
+  },
+  {
+    id: 14,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1727876294/Gallery/sgvnrkqmsykcv4xjr9np.jpg",
+  },
+  {
+    id: 15,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986108/Gallery/vvt2im2kpsbbqjq6hhfg.jpg",
+  },
+  {
+    id: 16,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986107/Gallery/xdbkjcupuywr3efl1ik6.jpg",
+  },
+  {
+    id: 17,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986107/Gallery/nahmntwq44xrwqaycvny.jpg",
+  },
+  {
+    id: 18,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/gbs9b1ryscomlmzepwpc.jpg",
+  },
+  {
+    id: 19,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/wdkibqxo9bh6xm0rsyzy.jpg",
+  },
+  {
+    id: 20,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/obloffhbagwrc8y5zbbl.jpg",
+  },
+  {
+    id: 21,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986106/Gallery/uhqkedvdfepausvn5vft.jpg",
+  },
+  {
+    id: 22,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986105/Gallery/z6gxyegvxfaz6s0ujg7x.jpg",
+  },
+  {
+    id: 23,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/zasxw5lqhn8shefhczsp.jpg",
+  },
+  {
+    id: 24,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/ofjqi0zff5yqhevjmr9o.jpg",
+  },
+  {
+    id: 25,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/zb3jr69jkxkjf52js7n9.jpg",
+  },
+  {
+    id: 26,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/cltkqv3tfbuconzlcjxe.jpg",
+  },
+  {
+    id: 27,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986104/Gallery/juxso5bb6rqpk1pijdzv.jpg",
+  },
+  {
+    id: 28,
+    imageUrl:
+      "https://res.cloudinary.com/dwlhesiyi/image/upload/v1730986103/Gallery/kyp2onqcxpwbgwoano8y.jpg",
+  },
 ];
 
 function Gallery() {
@@ -67,9 +176,11 @@ function Gallery() {
     }
   }, []);
   return (
-   <div className="bg-gray-100 py-16">
+    <div className="bg-gray-100 py-16">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center mb-12">Our Conferences and Publications</h2>
+        <h2 className="text-4xl font-bold text-center mb-12">
+          Our Conferences and Publications
+        </h2>
         <motion.div
           ref={galleryRef}
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
@@ -80,7 +191,7 @@ function Gallery() {
         </motion.div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
