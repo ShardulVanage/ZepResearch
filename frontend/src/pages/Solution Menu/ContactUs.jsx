73e0dbaa1de9e77082b53b054e2342b735d9ce0f
ShardulVanage <vsharadul@gmail.com>
2024-12-26 13:56:47 +0530

    seo policy added

--- frontend/src/pages/Solution Menu/ContactUs.jsx
+++ frontend/src/pages/Solution Menu/ContactUs.jsx
@@ -71,7 +71,7 @@ function ContactUs() {
                         <link rel="canonical" href="https://zepresearch.com" />
         </Helmet>
       <div className="bg-white">
-        Careers
+        
         <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
           <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
             <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg ">
