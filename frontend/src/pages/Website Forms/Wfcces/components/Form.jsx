956d9812b103e74a1d068ae489bd42c00b9f9a88
ShardulVanage <vsharadul@gmail.com>
2025-01-24 16:44:26 +0530

    unput type number

--- frontend/src/pages/Website Forms/Wfcces/components/Form.jsx
+++ frontend/src/pages/Website Forms/Wfcces/components/Form.jsx
@@ -103,7 +103,7 @@ export default function WfccesForm() {
                     Phone
                   </label>
                   <input
-                    type="tel"
+                    type="number"
                     id="Phone"
                     className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     required
