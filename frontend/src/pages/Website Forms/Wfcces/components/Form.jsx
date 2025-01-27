feddff28ce53c4dea3e9a53631e65eb7625b8ca6
ShardulVanage <vsharadul@gmail.com>
2025-01-27 16:17:55 +0530

    image change

--- frontend/src/pages/Website Forms/Wfcces/components/Form.jsx
+++ frontend/src/pages/Website Forms/Wfcces/components/Form.jsx
@@ -199,7 +199,7 @@ export default function WfccesForm() {
 
             <div className="relative ">
               <img
-                src={`https://plus.unsplash.com/premium_photo-1661764559869-f6052a14b4c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
+                src={`https://images.unsplash.com/photo-1574218732130-6b19257fd9db?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
                 alt="Contact support"
                 className="mb-8 rounded-3xl mx-auto"
               />
